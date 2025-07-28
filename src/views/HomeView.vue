94da07a3f940d39123d751cd836f012cd614e4b7
Asafe Toschi <asafeluiztoschi@gmail.com>
2025-07-28 15:17:12 -0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1 class="text-[56px]/[71px]">Heading (L)</h1>
+    <h1 class="uppercase text-[24px]/[31px]">PLAY VS PLAYER</h1>
+  </main>
+</template>
